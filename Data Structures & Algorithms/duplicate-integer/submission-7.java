255788036ca5455d859fc93a15c64cde79272fdb
AMRITH MULAKALA <adwaithamrith159@gmail.com>
2026-08-30 10:41:56 +0530

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        int i,j,n=nums.length;
+        Arrays.sort(nums);
+        for (i=0;i<n-1;i++){
+            for (j=i+1;j<n;j++)
+            {
+                if (nums[i]==nums[j])
+                return true;
+
+            }
+        }
+        return false;
+    }
+}
