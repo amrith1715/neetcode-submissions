8988708634430b65a667899192bc5a2ebd87f699
AMRITH MULAKALA <adwaithamrith159@gmail.com>
2026-09-13 21:57:44 +0530

    Add: special-array-i - submission-0

---
+++ Data Structures & Algorithms/special-array-i/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        boolean status=false;
+        if(nums.length==0 || nums.length==1)
+        {
+            return true;
+        }
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]%2==0)
+            {
+                if(nums[i+1]%2!=0)
+                {
+                  status=true;
+                }
+                else 
+                {
+                    status=false;
+                    break;
+                }
+            }
+            else if(nums[i]%2!=0)
+            {
+                if(nums[i+1]%2==0)
+                {
+                  status=true;
+                }
+                 else 
+                {
+                    status=false;
+                    break;
+                }
+            }
+            
+        
+        }
+        return status;
+        
+    }
+}
